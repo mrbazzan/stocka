9b5bacfb7df74df0025a9ebd676dfeeb953b33a9
Ayomisco <fayomide324@gmail.com>
2021-06-22 04:48:06 +0100

    Updated the model

--- Backend/apis/models.py
+++ Backend/apis/models.py
@@ -1,26 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionManager, BaseUserManager
-from django.db.models.fields import CharField, EmailField
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, User
+from django.db.models.fields import CharField
 
+# Create your models here.
 
 # This will helps us to v=create user. Overriding user manager
 
+
 class UserAccountManager(BaseUserManager):
     def create_user(self, email, first_name, last_name, phone_number, password=None):
-        # Error for not imputing and email account
+        # Error for not creating an email
         if not email:
             raise ValueError('Users must have an email address')
 
-        # This normalise turns the email to small caps incase enter in capital letters
+        # This normalise turns the email to small caps
         email = self.normalize_email(email)
-        user = self.model(email=email, first_name=first_name, last_name=last_name, phone_number=phone_number)
+        user = self.model(email=email, first_name=first_name,
+                          last_name=last_name, phone_number=phone_number)
 
         # Creating and using django password ashing
         user.set_password(password)
         user.save()
 
-# Create your models here.
-class UserAccount(AbstractBaseUser, PermissionManager):
+        return user
+    # def create_superuser()
+
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -43,7 +49,7 @@ class UserAccount(AbstractBaseUser, PermissionManager):
         return self.last_name
 
     def get_phone_number(self):
-        return self.pnone_number
+        return self.phone_number
 
     def __str__(self):
         return self.email
