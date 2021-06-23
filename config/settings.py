4e013249cc78a01742fcc9152ff6bb46e66b2f46
Bazzan <bazzanapalowow@gmail.com>
2021-06-24 00:19:33 +0100

    ch/restructure project directory - included .gitignore - set environmental variables - added new migrations to model [Delivers #Structure-Change]

--- config/settings.py
+++ config/settings.py
@@ -88,15 +88,11 @@ DATABASES = {
 }
 
 # EMAIL
-# Email Account: fayomide@gmail.com
-# App Password: password123
-
-EMAIL_BACKEND = os.environ.get('DJANGO_EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')  # 'ogtconsult21@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')  # "yywularwtjnqmkwb"
+EMAIL_HOST_USER = 'ogtconsult21@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 
 # Password validation
