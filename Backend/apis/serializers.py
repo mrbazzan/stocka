48f56e4c74b57aa998418fdee993ba6b9ef384dd
Bazzan <bazzanapalowow@gmail.com>
2021-06-23 10:54:27 +0100

    ch/restructure project directory - included .gitignore - added new migrations to apis [Delivers #Structure-Change]

--- Backend/apis/serializers.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth import get_user_model
-from djoser.serializers import UserCreateSerializer
-user = get_user_model()
-
-class UserCreateSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
-        model = user
-        fields = ('id', 'email', 'first', 'last_name', 'phone_number', 'password')
