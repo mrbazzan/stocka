7cfc710d25346829720e0d2e5a9b11c8cb2fa4fe
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2022-04-14 10:07:44 +0100

    stocka/settings: remove duplicate variable

--- stocka/settings.py
+++ stocka/settings.py
@@ -150,7 +150,6 @@ USE_TZ = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
